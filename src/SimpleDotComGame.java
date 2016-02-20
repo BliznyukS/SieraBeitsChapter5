60c2832f7ecf4c959c43ae259ba03ff2c9424e34
BliznyukS <bliznyuk.sergey@gmail.com>
2016-02-20 21:43:05 +0200

    Simple DotCom Game  completed

--- src/SimpleDotComGame.java
+++ src/SimpleDotComGame.java
@@ -1,3 +1,26 @@
 public class SimpleDotComGame {
 
+    public static void main(String[] args) {
+        int numGuesses = 0;
+
+        GameHelper helper = new GameHelper();
+
+        SimpleDotCom theDotCom = new SimpleDotCom();
+
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+
+        while (isAlive == true) {
+            String guess = helper.getUserInput("Введите число");
+            String result = theDotCom.checkYourself(guess);
+            numGuesses++;
+
+            if (result.equals("Потопил")) {
+                isAlive = false;
+                System.out.println("Вам потребовалось " + numGuesses + "попыток(и)");
+            }
+        }
+    }
 }
